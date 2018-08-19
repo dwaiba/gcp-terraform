a3c775ee5a2ee23dbb50820a6a0163cf4638612b
Dwai Banerjee <dwaiba@users.noreply.github.com>
2018-08-19 19:06:00 +0200

    Update compute_instances.tf

--- compute_instances.tf
+++ compute_instances.tf
@@ -2,7 +2,7 @@ resource "google_compute_disk" "default" {
   name  = "test-disk"
   type  = "pd-ssd"
   zone = "${var.zone}"
-  size = 100
+  size = 50
 }
 resource "google_compute_instance" "web" {
   count        = 1
