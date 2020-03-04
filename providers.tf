b457947c0b89dd351292a64890d58c2e37ad8efb
Dwai Banerjee <dwai@cloudgear.io>
2020-03-05 00:16:50 +0530

    Capture Ephemeral IPs and add Rancher

--- providers.tf
+++ providers.tf
@@ -1,5 +1,11 @@
 provider "google" {
+  version     = "3.11.0"
   credentials = file("credentials/google.json")
   project     = var.projectname
   region      = var.region
+}
+terraform {
+  required_providers {
+    google = "~> 3.11.0"
+  }
 }
